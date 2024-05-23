8b45da92ee878195899a144705c8b0bb1e5ad850
davide.g <davide.garrido@wisely.cl>
2024-05-23 11:45:16 -0400

    get port from .env and implement basic login endpoint

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-
-// Requiring module
 const express = require('express');
 const cors = require("cors");
 const routes = require("./routes/index")
 const { config } = require('./config/EnvConfig');
 const whitelist = config.whiteList;
-// Creating express object
+const port = config.port || 5000
+
 const app = express();
-
-
-
 
 app.use(express.json())
 
- 
-// Handling GET request
 app.get('/', (req, res) => { 
     res.send('A simple Node App is '
         + 'running on this server' + 'hola profes somos el grupo 3 de computacion distribuida y le presentamos el bakend 🕺') 
     res.end() 
 }) 
+
+
+routes(app)
  
-// Port Number
-const PORT = process.env.PORT ||5000;
- 
-// Server Setup
-app.listen(PORT,console.log(
-  `Server started on port ${PORT}`));
+app.listen(port,console.log(
+  `Server started on port ${port}`));
