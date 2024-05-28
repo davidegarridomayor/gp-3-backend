05d3f1e8cd5bbaae378bcd976c27d904183ec68a
davide.g <davide.garrido@wisely.cl>
2024-05-28 11:15:14 -0400

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authRouter = require('./v1/AuthRouter');
+const usersRouter = require('./v1/UserRouter')
 const { swaggerDocs } = require('./v1/swagger');
 //const { isAuthenticated } = require('../middlewares/AuthHandler');
 
 function routes(app) {
   const router = express.Router();
   app.use('/api/v1', router);
-  //router.use('/users', usersRouter);
+  router.use('/users', usersRouter);
   router.use('/auth', authRouter);
   router.use('/docs', ...swaggerDocs); // Spread the swaggerDocs array to use the middleware functions
 }
