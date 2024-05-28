2ede49e342396db42b7d8a2133c3b4881173f5e1
davide.g <davide.garrido@wisely.cl>
2024-05-28 13:20:22 -0400

    send error as body response

--- routes/v1/AuthRouter.js
+++ routes/v1/AuthRouter.js
@@ -48,7 +48,7 @@ router.post("/login", async (req, res, next) => {
     res.status(200).json(await userService.login(user));
   } catch (error) {
     console.log('error', error);
-    res.status(500).send('Internal Server Error', error);
+    res.status(500).send({ error: 'Internal Server Error', message: error.message });
   }
 });
 
