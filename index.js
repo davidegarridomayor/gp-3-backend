4b09065f7c790baede4ba8899f051c80479bc431
davide.g <davide.garrido@wisely.cl>
2024-05-28 12:18:56 -0400

    add cors

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const app = express();
 
 app.use(express.json())
 
+const options = {
+    origin: (origin, callback) => {
+      // CHANGE THIS LINE TO ALLOW ACCESS ONLY FROM YOUR FRONTEND
+      if (whitelist.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed'));
+      }
+    }
+  }
+
+app.use(cors(options));
 app.get('/', (req, res) => { 
     res.send('A simple Node App is '
         + 'running on this server' + 'hola profes somos el grupo 3 de computacion distribuida y le presentamos el bakend 🕺') 
