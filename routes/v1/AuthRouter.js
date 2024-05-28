e10e9131715b15cdae2a633250befae01dfddb8b
davide.g <davide.garrido@wisely.cl>
2024-05-28 11:12:44 -0400

    update login endpoint and add register/add users endpoint

--- routes/v1/AuthRouter.js
+++ routes/v1/AuthRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+const UserService = require("./../../services/v1/UserService");
+const userService = new UserService();
 /**
  * @swagger
  * /auth/login:
@@ -17,10 +18,17 @@ const router = express.Router();
  *             properties:
  *               username:
  *                 type: string
- *                 example: "user@example.com"
+ *                 example: "username"
  *               password:
  *                 type: string
  *                 example: "password123"
+ *               token:
+ *                 type: string
+ *                 example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjgsInVzZXJuYW1lIjoibmV3dXNlckFBQTEyMyIsImlhdCI6MTcxNjkwODk1OCwiZXhwIjoxNzE2OTA5MjU4fQ.kWqPVXSpjR-EdrW7qBw--rg0obWy9W92MNvOXYvxWcM"
+ *               tokenExpiration:
+ *                 type: date
+ *                 example: "2024-05-28T15:14:18.764Z"
+ * 
  *     responses:
  *       200:
  *         description: Login successful
@@ -36,9 +44,57 @@ const router = express.Router();
 
 router.post("/login", async (req, res, next) => {
   try {
-    res.status(200).json({ message: "Login successful" });
+    const { body: user } = req;
+    res.status(200).json(await userService.login(user));
   } catch (error) {
     next(error);
+  }
+});
+
+
+
+/**
+ * @swagger
+ * /auth/:
+ *   post:
+ *     summary: Register/Create a user
+ *     tags: 
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: "username"
+ *               password:
+ *                 type: string
+ *                 example: "password123"
+ *     responses:
+ *       201:
+ *         description: User created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                     type: int
+ *                     example: 1
+ *                 username:
+ *                     type: string
+ *                     example: "newuser"
+ *                 
+ */
+router.post("/register", async (req, res, next) => {
+  try {
+      const { body: user } = req;
+      res.status(201).json(await userService.add(user));
+  } catch (error) {
+      next(error);
   }
 });
 
